a2e933efbf9fc9e82531cbed75543ade4ff8de28
mayuresh1606 <mayureshovhal16@gmail.com>
2021-07-28 17:37:40 +0530

    Attempt to link postgres

--- venture_world/settings.py
+++ venture_world/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -100,6 +101,8 @@ DATABASES = {
     }
 
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
